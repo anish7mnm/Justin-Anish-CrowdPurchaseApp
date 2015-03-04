071efe225761a3e013b1b3815f4aa91f2b736034
github username <github email address>
2015-03-04 15:38:58 -0500

    Project Begins

---
+++ JACrowdPurchasingApp/JACrowdPurchasingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JACrowdPurchasingApp
+//
+//  Created by Anish Kumar on 3/4/15.
+//  Copyright (c) 2015 Anish Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JACrowdPurchasingApp/JACrowdPurchasingApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JACrowdPurchasingApp
+//
+//  Created by Anish Kumar on 3/4/15.
+//  Copyright (c) 2015 Anish Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
