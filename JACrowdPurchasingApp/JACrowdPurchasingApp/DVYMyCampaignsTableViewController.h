3312f212767e51d4a6164b5e623db134e6d94472
taesongkim <taesongkim@gmail.com>
2015-03-23 13:44:25 -0400

    added some objects and methods

---
+++ JACrowdPurchasingApp/JACrowdPurchasingApp/DVYMyCampaignsTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  DVYMyCampaignsTableViewController.h
+//  JACrowdPurchasingApp
+//
+//  Created by Justin on 3/22/15.
+//  Copyright (c) 2015 Anish Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DVYMyCampaignsTableViewController : UITableViewController
+
+
+
+@end
